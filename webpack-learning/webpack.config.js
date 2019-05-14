011f97ce21f57f94eab0fc5b4ee88a86640d881f
helloworld-liushijie <1690150504@qq.com>
2019-05-14 23:45:36 +0800

    webpack使用

--- webpack-learning/webpack.config.js
+++ webpack-learning/webpack.config.js
@@ -6,5 +6,21 @@ module.exports = {
     //打包生成文件
     output: {
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
     }
 }
